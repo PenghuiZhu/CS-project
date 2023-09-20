b3f9a1b37fd23a36b87b6e80a46f00e2c8fd143c
Penghui Zhu <penghuizhu1109@gmail.com>
2023-09-20 17:32:14 -0500

    front-end

---
+++ front-end/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login(){
+    return(
+        <div>
+            <h1>login</h1>
+            <input className="account"></input>
+            <input type="text"></input>
+        </div>
+    )
+}
